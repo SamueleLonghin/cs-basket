cf43811b47e6547b6b72fcfe0cf8008148ec6627
Samuele <longhin.samuele@outlook.com>
2021-12-19 18:30:05 +0100

    env

--- config/db.php
+++ config/db.php
@@ -1,12 +1,17 @@
 <?php
 
+$port = getenv("MYSQL_DBPORT");
+$host = getenv("MYSQL_DBHOST");
+$user = getenv("MYSQL_DBUSER");
+$password = getenv("MYSQL_DBPASS");
+
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=mysql:3306;dbname=basket',
-    'username' => 'root',
-    'password' => 'secret',
+    'dsn' => "mysql:host=$host:$port;dbname=basket",
+    'username' => $user,
+    'password' => $password,
     'charset' => 'utf8',
-    
+
     // Schema cache options (for production environment)
     //'enableSchemaCache' => true,
     //'schemaCacheDuration' => 60,
