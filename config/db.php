2aef83e137d7649d636416309389fec988667855
Samuele <46517478+SamueleLonghin@users.noreply.github.com>
2021-12-20 08:32:20 +0100

    Update db.php

--- config/db.php
+++ config/db.php
@@ -4,10 +4,11 @@ $port = getenv("MYSQL_DBPORT");
 $host = getenv("MYSQL_DBHOST");
 $user = getenv("MYSQL_DBUSER");
 $password = getenv("MYSQL_DBPASS");
+$db_name = getenv("MYSQL_DBNAME");
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => "mysql:host=$host:$port;dbname=basket",
+    'dsn' => "mysql:host=$host:$port;dbname=$db_name",
     'username' => $user,
     'password' => $password,
     'charset' => 'utf8',
